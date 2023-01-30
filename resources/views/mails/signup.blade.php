2548bea6d452674f525ae78b87f105ebfe321d54
Quint Legius <quint.legius@gmail.com>
2023-01-31 00:05:58 +0100

    removed verified mails

--- resources/views/mails/signup.blade.php
+++ resources/views/mails/signup.blade.php
@@ -6,10 +6,6 @@ Bedankt voor het inschrijven voor de Salve Mundi introductie!
 
 Binnenkort ontvang je per mail meer informatie over de introductie.
 
-Wel vragen we je om je mail te verifiëren via de volgende link:
-
-{{ env('APP_URL') }}inschrijven/verify/{{ $verificationToken->id }}
-
 Alvast bedankt!
 
 Met vriendelijke groet,
