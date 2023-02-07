4a7ca6f566b413b699ced39cb0287fafad8c587a
Quint Legius <quint.legius@gmail.com>
2023-02-07 19:22:32 +0100

    added registrations back

--- resources/views/admin/participants.blade.php
+++ resources/views/admin/participants.blade.php
@@ -63,6 +63,13 @@ setActive("participants");
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
                     <li>
+                        <form method="POST" action="/registrations">
+                            @csrf
+                            <button type="submit" class="dropdown-item">Stuur betaling email</button>
+                        </form>
+                    </li>
+
+                    <li>
                         <form method="POST" action="/participants/resendQRcode">
                             @csrf
                             <button type="submit" class="dropdown-item">Stuur QR-code kiddos</button>
