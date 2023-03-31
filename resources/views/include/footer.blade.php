9477ca207301b690b8ae11568eac1bc8af9229a3
Quint Legius <quint.legius@gmail.com>
2023-03-31 17:49:04 +0200

    added development alert

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,9 +1,9 @@
-<div class="p-2">
-  &nbsp;
-</div>
 <footer class="text-white text-center text-lg-start fixed-bottom footer-sv">
     <div class="text-center p-2">
-        Copyright &copy; <?php echo date("Y"); ?>&nbsp;
+        Copyright &copy; {{ date("Y") }}
       <a class="text-white" href="https://salvemundi.nl/" target="_blank">salvemundi.nl</a>
+        @if(env('APP_ENV') === 'local' || env('APP_ENV') === 'dev' || env('APP_ENV') === 'development')
+            <span class="badge rounded-pill text-bg-danger text-white">DEVELOPMENT</span>
+        @endif
     </div>
 </footer>
