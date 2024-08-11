fcdf8e7a23e87825632c458b3c30de65cd729f03
Quint Legius <quint.legius@gmail.com>
2024-08-11 16:01:50 +0200

    ui friendliness

--- resources/views/admin/planning/modals/shifts.blade.php
+++ resources/views/admin/planning/modals/shifts.blade.php
@@ -5,10 +5,10 @@
                 @csrf
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="exampleModalLabel">Diensten</h1>
+                    <button type="button" class="btn btn-primary mb-2 ms-2" onclick="addShift()"><i class="fas fa-plus"></i></button>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body" style="max-height: 70vh; overflow-y: auto;">
-                    <button type="button" class="btn btn-primary mb-2" onclick="addShift()"><i class="fas fa-plus"></i></button>
                     <div id="shiftsContainer">
                         <hr class="my-3">
                         @foreach($shifts as $index => $object)
